8d8c36de600e189f22a3536150a95d13016528b4
gtran6 <gtran6@depaul.edu>
2023-06-01 14:07:26 -0500

    commit

--- app/src/main/java/com/myprojects/ecommercestore/data/repo/ProductRepo.kt
+++ app/src/main/java/com/myprojects/ecommercestore/data/repo/ProductRepo.kt
@@ -1,13 +1,14 @@
 package com.myprojects.ecommercestore.data.repo
 
 import com.myprojects.ecommercestore.data.api.ApiInterface
+import com.myprojects.ecommercestore.data.local.FavoriteDao
 import com.myprojects.ecommercestore.model.ApiResponse
 import com.myprojects.ecommercestore.model.ApiResponseItem
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class ProductRepo @Inject constructor(val apiInterface: ApiInterface) {
+class ProductRepo @Inject constructor(val apiInterface: ApiInterface, val favDao: FavoriteDao) {
 
     fun getAllProductsFromServer(sort: String) : Flow<ApiResponse> = flow {
         emit(apiInterface.getProductList(sort))
@@ -16,4 +17,10 @@ class ProductRepo @Inject constructor(val apiInterface: ApiInterface) {
     fun getSingleProductFromServer() : Flow<ApiResponseItem> = flow {
         emit(apiInterface.getSingleProduct())
     }
+
+    suspend fun insertItem(item: ApiResponseItem) = favDao.insert(item)
+
+    suspend fun deleteItem(item: ApiResponseItem) = favDao.delete(item)
+
+    fun getAllItems() = favDao.getFavoriteItems()
 }
